bfee6897eb6af57c9896c30c88d430af30be3d93
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-02-21 22:54:23 +0300

    Create 0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module provides a simple addition function with type annotations.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats.
+    """
+    return a + b
